09d5d66ff1ec0d8c498f18365ed5b29b3e857401
KevinJara123 <kevinemanueljara796@gmail.com>
2022-09-27 01:58:05 -0300

    Mas avances

---
+++ js/correo.js
@@ -0,0 +1,15 @@
+
+    /* Contactos*/
+
+    const $form=document.querySelector('#form')
+    const $buttonMailto=document.querySelector('#email-1')
+    
+    $form.addEventListener('submit',handleSubmit)
+
+    function handleSubmit(event){
+      event.preventDefault()
+      const form =new FormData(this)
+      $buttonMailto.setAttribute('href',`mailto:federacion_cordoba_bsr@hotmail.com?subject=${form.get('name')}&body=${form.get('message')}`)
+      $buttonMailto.click();
+
+    }
