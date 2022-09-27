af2edcec4427cd282abb9164545165bfe954776d
KevinJara123 <kevinemanueljara796@gmail.com>
2022-09-27 18:15:09 -0300

    Hola

--- js/correo.js
+++ js/correo.js
@@ -1,15 +1,36 @@
+function enviarCorreo(){
+  $(".alerta_correo").css('display','none');
+  $(".alerta_mensaje").css('display','none');
+  $('.alerta_correcto').css('display','none');
+  $('.alerta_incorrecto').css('display','none');
+  var correo=$("#correo").val();
+  var mensaje=$("#mensaje").val();
+  var validacion_correo = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+  if(!validacion_correo.test(correo)){
+    $(".alerta_correo").css('display','block');
+    valido=0;
+  }
+  if(mensaje.length <=5){
+    $(".alerta_mensaje").css('display','block');
+    valido=0;
+  }
+  if(valido == 1){
+    var datos = 'correo='+correo+'&mensaje='+mensaje;
+    $.ajax({
+      type:"POST",
+      url:"enviar.php",
+      data:datos,
+      success:function(res){
+        if(parseIntI(res)==1){
+          $('.alerta_correcto').css('display','block');
+        }else{
+          $('.alerta_incorrecto').css('display','block');
+        }
+      },
+      error: function(res){
 
-    /* Contactos*/
+      }
+    });
+  }
 
-    const $form=document.querySelector('#form')
-    const $buttonMailto=document.querySelector('#email-1')
-    
-    $form.addEventListener('submit',handleSubmit)
-
-    function handleSubmit(event){
-      event.preventDefault()
-      const form =new FormData(this)
-      $buttonMailto.setAttribute('href',`mailto:federacion_cordoba_bsr@hotmail.com?subject=${form.get('name')}&body=${form.get('message')}`)
-      $buttonMailto.click();
-
-    }
+}
